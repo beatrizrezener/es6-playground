69632e91165abd653a5a65125343703be6e6cd29
Beatriz <beatrizrezener@gmail.com>
2019-12-29 11:39:13 -0300

    destructing

--- bundle.js
+++ bundle.js
@@ -83,4 +83,27 @@ var arrowFunctionReturningObject = function arrowFunctionReturningObject() {
   };
 };
 
-console.log(arrowFunctionReturningObject());
+console.log(arrowFunctionReturningObject()); // destructing
+
+var user = {
+  name: 'Mariah',
+  age: '45',
+  address: {
+    city: 'Brasilia',
+    state: 'DF'
+  }
+};
+var name = user.name,
+    age = user.age,
+    city = user.address.city;
+console.log(name);
+console.log(age);
+console.log(city);
+
+function showName(_ref) {
+  var name = _ref.name,
+      age = _ref.age;
+  console.log(name, age);
+}
+
+showName(user);
--- main.js
+++ main.js
@@ -61,3 +61,23 @@ console.log(arrowFunction());
 
 const arrowFunctionReturningObject = () => ({ name: 'Mariah' });
 console.log(arrowFunctionReturningObject());
+
+// destructing
+const user = {
+  name: 'Mariah',
+  age: '45',
+  address: {
+    city: 'Brasilia',
+    state: 'DF',
+  }
+}
+
+const { name, age, address: { city } } = user;
+console.log(name);
+console.log(age);
+console.log(city);
+
+function showName({ name, age }) {
+  console.log(name, age);
+}
+showName(user);
