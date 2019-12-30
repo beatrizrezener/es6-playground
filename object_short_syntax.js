155c5daf0c06c87194aa221f81d7ccf251b3bb02
Beatriz <beatrizrezener@gmail.com>
2019-12-30 10:08:51 -0300

    object short syntax example

---
+++ object_short_syntax.js
@@ -0,0 +1,17 @@
+// const name = 'Mariah';
+// const age = 26;
+// const user = {
+//   name: name,
+//   age: age,
+//   city: 'Brasília',
+// };
+
+const name = 'Mariah';
+const age = 26;
+const user = {
+  name,
+  age,
+  city: 'Brasília',
+};
+
+console.log(user);
