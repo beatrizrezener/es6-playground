1d9bb9647c082d0d6cba808617087dd004baacee
Beatriz Rezener <beatrizrezener@gmail.com>
2020-01-25 08:30:39 -0300

    challenge: let

---
+++ src/challenges/let.js
@@ -0,0 +1,20 @@
+/* 
+ Change code to match output
+    BEFORE: 5
+    AFTER: 10
+ */
+
+ var i = 10;
+
+ for (var i = 0; i < 5; i++) {
+     // nothing to do
+ }
+
+ console.log(i);
+
+/*
+    We can change `var i` of the loop to `let i`, 
+    So the new `i` will be blocked scoped.
+*/
+ 
+
